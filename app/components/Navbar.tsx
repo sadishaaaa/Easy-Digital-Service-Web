0183aa8a6105092cb24ef17577f2907942e63889
sadishaaaa <sthasadisha2019@gmail.com>
2024-02-21 14:43:13 +0545

    responsive navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,18 @@
 "use client";
 import React, { useState } from "react";
-
+import { AiOutlineClose } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiArrowDropDownLine } from "react-icons/ri";
 type Props = {};
 
 const Navbar = (props: Props) => {
-  const [showProfile, setshowProfile] = useState<boolean>(false);
-  const [showNav, setShowNav] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
   return (
-    <div>
-      <div className="flex items-center justify-between py-4 relative">
+    <div className="bg-white shadow-md">
+      <div className="flex  px-5 max-w-[1280px] mx-auto items-center justify-between py-4 relative">
         <div className="flex items-center md:space-x-10 lg:space-x-20">
           <div className="font-semibold text-2xl">
             <a href="">LOGO</a>
@@ -16,18 +20,21 @@ const Navbar = (props: Props) => {
           <nav className="max-lg:hidden">
             <ul className="flex items-center lg:space-x-10 space-x-7 opacity-70 text-[1rem]">
               <li>
-                <a href="services" className="py-3 inline-block w-full">
+                <a href="services" className="flex items-center py-3 w-full">
                   Services
+                  <RiArrowDropDownLine size="1.5rem" />
                 </a>
               </li>
               <li>
-                <a href="industries" className="py-3 inline-block w-full">
+                <a href="industries" className="flex items-center  py-3 w-full">
                   Industries
+                  <RiArrowDropDownLine size="1.5rem" />
                 </a>
               </li>
               <li>
-                <a href="company" className="py-3 inline-block w-full">
+                <a href="company" className="flex items-center  py-3 w-full">
                   Company
+                  <RiArrowDropDownLine size="1.5rem" />
                 </a>
               </li>
               <li>
@@ -46,9 +53,56 @@ const Navbar = (props: Props) => {
             Book a Call
           </button>
           <div className="lg:hidden">
-            <span className="material-symbols-outlined">menu</span>
+            <GiHamburgerMenu size="2rem" onClick={toggleModal} />
           </div>
         </div>
+        {/* Modal */}
+        {showModal && (
+          <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center">
+            <div className="flex flex-col justify-start  h-[70vh] z-50 ">
+              <AiOutlineClose
+                size="1.5rem"
+                className="cursor-pointer"
+                color="white"
+                onClick={toggleModal}
+              />
+            </div>
+            <div className="bg-white w-[20rem] p-8 rounded-lg ">
+              <ul className="space-y-4 text-left">
+                <li>
+                  <a href="services" onClick={toggleModal}>
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a href="industries" onClick={toggleModal}>
+                    Industries
+                  </a>
+                </li>
+                <li>
+                  <a href="company" onClick={toggleModal}>
+                    Company
+                  </a>
+                </li>
+                <li>
+                  <a href="company" onClick={toggleModal}>
+                    Career
+                  </a>
+                </li>
+                <li>
+                  <a href="company" onClick={toggleModal}>
+                    Sucess Stories
+                  </a>
+                </li>
+                <li>
+                  <a href="contactus" onClick={toggleModal}>
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="px-5 max-w-[1280px] mx-auto">
+    <div className="">
       <Navbar />
     </div>
   );
